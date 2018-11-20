6e9ea2707318d28b0317ec6fdd645109a473be52
Muhun Kim <iam@muhun.kim>
2018-11-20 19:42:07 +0900

    Update DLfS

--- Deep Learning from Scratch/hanson/three_layer.py
+++ Deep Learning from Scratch/hanson/three_layer.py
@@ -8,21 +8,19 @@ def identity(x): # 항등 함수
 	return x
 
 def forward(net, x):
-	a1 = np.dot(x, net['W1']) + net['b1']
+	a1 = np.dot(x, net['W1']) + net['b1'] #1층
 	z1 = sigmoid(a1)
-	a2 = np.dot(z1, net['W2'])
+	a2 = np.dot(z1, net['W2']) + net['b2'] #2층
 	z2 = sigmoid(a2)
-	a3 = np.dot(z2, net['W3']) + net['b3']
-	y = identity(a3)
-	return y
+	a3 = np.dot(z2, net['W3']) + net['b2'] #3층
+	return identity(a3)
 
 network = {
 	'W1': np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]]),
 	'b1': np.array([0.1, 0.2, 0.3]),
 	'W2': np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]]),
 	'b2': np.array([0.1, 0.2]),
-	'W3':np.array([[0.1, 0.3], [0.2, 0.4]]),
-	'b3': np.array([0.1, 0.2])
+	'W3':np.array([[0.1, 0.3], [0.2, 0.4]])
 }
 
 x = np.array([1.0, 0.5])
