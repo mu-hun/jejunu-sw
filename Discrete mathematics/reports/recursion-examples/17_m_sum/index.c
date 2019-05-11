a0de467be4e47b9cb583a5cbe59f096ed4dc3053
Muhun Kim <iam@muhun.kim>
2019-05-11 17:29:57 +0900

    이산수힉: 16_m_sum 을 17_m_sum으로 병합

--- Discrete mathematics/reports/recursion-examples/17_m_sum/index.c
+++ Discrete mathematics/reports/recursion-examples/17_m_sum/index.c
@@ -1,33 +1,40 @@
 #include <stdio.h>
 
-#define MAXN 100
-#define min(x, y) ((x)<(y)?(x):(y))
-
-void m_sum(int n, int m, int num[], int l, int t)
+// copied
+void print_arr(int arr[], int arr_len)
 {
-    int i;
+	int i;
 
-    if (l == m) {
-        if (n != 0)
-            return;
-        for (i = 0; i < l; i++)
-            printf("%d ", num[i]);
-        printf("\n");
-        return;
-    }
-
-    for (i = 0; i <= min(t, n); i++) {
-        num[l] = i;
-        m_sum(n - i, m, num, l + 1, i);
-    }
+	for (i = 0; i < arr_len; i++)
+		printf("%d ", arr[i]);
+	printf("\n");
 }
+
+void m_sum(int n, int m, int num[], int index)
+{
+	int i;
+
+	if (index == m) {
+		if (n == 0) {
+			print_arr(num, index);
+		}
+		return;
+	}
+
+	for (i = 1; i <= n; i++) {
+		num[index] = i;
+		m_sum(n - i, m, num, index + 1);
+	}
+}
+
+#define MAXN 100
 
 int main()
 {
-    int num[MAXN], n, m;
+	int num[MAXN], n, m;
 
-    printf("input n, m: ");
-    scanf("%d %d", &n, &m);
-    m_sum(n, m, num, 0, n);
-    return 0;
+	printf("input n, m: ");
+	scanf("%d %d", &n, &m);
+	m_sum(n, m, num, 0);
+	return 0;
 }
