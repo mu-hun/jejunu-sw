5d11d1617d8bff6cde48d49b90b428277abbd2c7
Muhun Kim <iam@muhun.kim>
2019-05-11 21:20:19 +0900

    이진탐색 오류 고침
    중간점 : (L + R) / 2

--- Discrete mathematics/reports/recursion-examples/20_binary_search/index.c
+++ Discrete mathematics/reports/recursion-examples/20_binary_search/index.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
 int binary_search(int arr[], int L, int R, int x) {
-    int mid = L + (R-1) / 2;
-    if (arr[mid] == x) return mid;
-    if (arr[mid] > x) binary_search(arr, L, mid-1, x);
-    if (arr[mid] < x) binary_search(arr, mid+1, R, x);
+    if(L <= R) {
+        int mid = (L + R)/2;  /*low + (high - low)/2;*/
+
+        if((mid == 0 || x > arr[mid-1]) && (arr[mid] == x))
+            return mid;
+        if(x > arr[mid])
+            return binary_search(arr, (mid + 1), R, x);
+        return binary_search(arr, L, (mid -1), x);
+    }
     return -1;
 }
 
@@ -13,9 +18,9 @@ int main(void)
     int arr[] = { 2, 3, 4, 10, 40 };
     int n = sizeof(arr) / sizeof(arr[0]);
     int x = 10;
-    int result = binarySearch(arr, 0, n - 1, x);
+    int result = binary_search(arr, 0, n - 1, x);
     (result == -1) ? printf("Element is not present in array")
                    : printf("Element is present at index %d",
                             result);
     return 0;
-} 
+}
