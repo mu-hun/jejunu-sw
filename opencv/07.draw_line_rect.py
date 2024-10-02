f63eef6ca6af7dae49e2386aa63d9c4b4845c490
Muhun Kim <iam@muhun.kim>
2024-10-02 10:44:05 +0900

    Draw line and Rect

--- opencv/07.draw_line_rect.py
+++ opencv/07.draw_line_rect.py
@@ -1,23 +1,23 @@
 import numpy as np
 import cv2
 
-blue, green, red = 
-image = 
-image[:] = 
+blue, green, red = (255, 0, 0), (0, 255, 0), (0, 0, 255)
+image = np.zeros((400, 600, 3), np.uint8)  # 영상 생성
+image[:] = (255, 255, 255)  # 흰색으로 초기화
 
-pt1, pt2 =  
-pt3, pt4 =
-roi = 50, 200, 200, 100
+pt1, pt2 = (50, 50), (250, 350)
+pt3, pt4 = (400, 150), (500, 50)
+roi = (50, 200, 200, 100)
 
 # 직선 그리기
-cv2.line()
-cv2.line()    			# 
+cv2.line(image, pt1, pt2, red)
+cv2.line(image, pt3, pt4, green, 3, cv2.LINE_AA)
 
 # 사각형 그리기
-cv2.rectangle()             # 
-cv2.rectangle( )                  # 
-cv2.rectangle( )  # 내부 채움
+cv2.rectangle(image, pt1, pt2, blue, 3, cv2.LINE_4)
+cv2.rectangle(image, roi, red, 3, cv2.LINE_8)
+cv2.rectangle(image, (400, 200, 100, 100), green, cv2.FILLED)
 
-cv2.imshow('Line & Rectangle', image)							# 
+cv2.imshow("Line & Rectangle", image)
 cv2.waitKey(0)
-cv2.destroyAllWindows()											# 
+cv2.destroyAllWindows()
