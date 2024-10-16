fb379e72fcd715fc2bf8cf59c2e6c54ce6ed57b2
Muhun Kim <iam@muhun.kim>
2024-10-16 09:50:30 +0900

    Implement Camera IO

--- opencv/15.write_image1.py
+++ opencv/15.write_image1.py
@@ -8,8 +8,8 @@ params_jpg = (cv2.IMWRITE_JPEG_QUALITY, 10)  #
 params_png = [cv2.IMWRITE_PNG_COMPRESSION, 9]
 
 ## 행렬을 영상 파일로 저장
-cv2.imwrite("images/write_test1.jpg", image)  #
-cv2.imwrite("images/write_test2.jpg", image, params_jpg)  #
-cv2.imwrite("images/write_test3.png", image, params_png)
-cv2.imwrite("iamges/write_test4.bmp", image)  #
+cv2.imwrite("images/output/test1.jpg", image)  #
+cv2.imwrite("images/output/test2.jpg", image, params_jpg)  #
+cv2.imwrite("images/output/test3.png", image, params_png)
+cv2.imwrite("iamges/output/test4.bmp", image)  #
 print("저장 완료")
--- opencv/17.read_pccamera.py
+++ opencv/17.read_pccamera.py
@@ -1,29 +1,36 @@
 import cv2
 
-def put_string(frame, text, pt, value, color=(120, 200, 90)):             # 문자열 출력 함수 - 그림자 효과
+
+def put_string(
+    frame, text, pt, value, color=(120, 200, 90)
+):  # 문자열 출력 함수 - 그림자 효과
     text += str(value)
     shade = (pt[0] + 2, pt[1] + 2)
     font = cv2.FONT_HERSHEY_SIMPLEX
     cv2.putText(frame, text, shade, font, 0.7, (0, 0, 0), 2)  # 그림자 효과
     cv2.putText(frame, text, pt, font, 0.7, (120, 200, 90), 2)  # 글자 적기
 
+
 capture = cv2.VideoCapture(0)  # 0번 카메라 연결
 if capture.isOpened() == False:
     raise Exception("카메라 연결 안됨")
 
 # 카메라 속성 획득 및 출력
-print( )
+print("너비 %d" % capture.get(cv2.CAP_PROP_FRAME_WIDTH))
 print("높이 %d" % capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
 print("노출 %d" % capture.get(cv2.CAP_PROP_EXPOSURE))
-print( )
+print("밝기 %d" % capture.get(cv2.CAP_PROP_BRIGHTNESS))
 
-while True:  # 무한 반복
-    
+while True:
+    ret, frame = capture.read()
+    if not ret:
+        break
 
+    put_string(frame, "EXPOS:", (10, 40), capture.get(cv2.CAP_PROP_EXPOSURE))
 
-
-
-    
     title = "View Frame from Camera"
-    cv2.imshow(title, frame)  # 윈도우에 영상 띄우기
+    cv2.imshow(title, frame)
+    if cv2.waitKey(1) & 0xFF == ord("q"):  # 'q' 키를 누르면 종료
+        break
 capture.release()
+cv2.destroyAllWindows()
