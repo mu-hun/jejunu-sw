4db51e865f81b9e7a207d72cfa016957c804c133
Muhun Kim <iam@muhun.kim>
2024-10-31 14:11:20 +0900

    중간시험 전용 주석 추가

--- opencv/10.draw_ellipse.py
+++ opencv/10.draw_ellipse.py
@@ -10,9 +10,12 @@ size = (120, 60)  # 타원 크기 - 반지름 값임
 cv2.circle(image, pt1, 1, 0, 2)  # 타원 중심점
 cv2.circle(image, pt2, 1, 0, 2)
 
-cv2.ellipse(image, pt1, size, 0, 0, 360, blue, 1)
+# 숙지 TODO
+# 3시 방향이 0도이며 시계방향으로 증가
+# 이미지, 중심점, (x, y) 축 반지름, 각도, 호 시작각도, 호 종료각도, 색상, 선두께
+cv2.ellipse(image, pt1, size, 30, 90, 360, blue, 1)
 cv2.ellipse(image, pt2, size, 90, 0, 360, blue, 1)
-cv2.ellipse(image, pt1, size, 0, 30, 270, orange, 4)
+# cv2.ellipse(image, pt1, size, 0, 30, 270, orange, 4)
 cv2.ellipse(image, pt2, size, 90, -45, 90, orange, 4)
 
 cv2.imshow("Draw Eclipse & Arc", image)
--- opencv/chap05/04.arithmethic_op copy.py
+++ opencv/chap05/04.arithmethic_op copy.py
@@ -3,7 +3,9 @@ import numpy as np, cv2
 m1 = np.full((3, 6), 10, np.uint8)
 m2 = np.full((3, 6), 50, np.uint8)
 m_mask = np.zeros(m1.shape, np.uint8)
-m_mask[:, 3:] = 1  # 관심 영역을 지정한 후, 1 할당, 시험에 나오기 좋은 문제!!
+
+# 모든 행 범위⬇️의 열 3 인덱스 ➡️ 부터 끝까지 1로 설정
+m_mask[:, 3:] = 1  # 시험에 나오기 좋은 문제!!
 
 m_add1 = cv2.add(m1, m2)
 m_add2 = cv2.add(m1, m2, mask=m_mask)
