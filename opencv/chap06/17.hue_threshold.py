c2e94b8a0c4336a8f5540b15464fdde0f26e7b46
Muhun Kim <iam@muhun.kim>
2024-11-07 15:52:52 +0900

    이미지 임계처리

--- opencv/chap06/17.hue_threshold.py
+++ opencv/chap06/17.hue_threshold.py
@@ -1,20 +1,31 @@
-import numpy as np, cv2
-
-def onThreshold(value):
-    
+import cv2
 
 
-
-    cv2.imshow("result", result)
-
-BGR_img = cv2.imread("Source/chap06/images/color_space.jpg", cv2.IMREAD_COLOR) # 컬러 영상 읽기
-if BGR_img is None: raise Exception("영상 파일 읽기 오류")
+window_name = "HSV & Hue Threshold"
 
 
+def on_threshold_change(value):
+    th1 = cv2.getTrackbarPos("Hue_th1", window_name)
+    th2 = cv2.getTrackbarPos("Hue_th2", window_name)
+
+    _, result = cv2.threshold(hue_channel, th2, 255, cv2.THRESH_TOZERO_INV)
+    cv2.threshold(result, th1, 255, cv2.THRESH_BINARY, result)
+    cv2.imshow(window_name, result)
 
 
+BGR_img = cv2.imread("chap06/images/color_space.jpg", cv2.IMREAD_COLOR)
+if BGR_img is None:
+    raise Exception("Error reading image file")
 
+HSV_img = cv2.cvtColor(BGR_img, cv2.COLOR_BGR2HSV)
 
+hue_channel = HSV_img[:, :, 0]
 
-cv2.imshow("BGR_img", BGR_img)
+cv2.namedWindow(window_name)
+cv2.createTrackbar("Hue_th1", window_name, 50, 255, on_threshold_change)
+cv2.createTrackbar("Hue_th2", window_name, 100, 255, on_threshold_change)
+
+cv2.imshow("BGR Image", BGR_img)
+
 cv2.waitKey(0)
+cv2.destroyAllWindows()
--- opencv/chap06/18.thresholding.py
+++ opencv/chap06/18.thresholding.py
@@ -1,16 +1,16 @@
 import cv2
 
-def onThreshold(value):
-    
+
+def onThreshold(value: int):
+    result = cv2.threshold(image, value, 255, cv2.THRESH_BINARY)[1]
     cv2.imshow("result", result)
 
 
-image = cv2.imread("Source/chap06/images/color_space.jpg", cv2.IMREAD_GRAYSCALE) # 컬러 영상 읽기
-if image is None: raise Exception("영상 파일 읽기 오류")
+image = cv2.imread("chap06/images/color_space.jpg", cv2.IMREAD_GRAYSCALE)
+if image is None:
+    raise Exception("영상 파일 읽기 오류")
 
 cv2.namedWindow("result")
-
-
-
+cv2.createTrackbar("threshold", "result", 128, 255, onThreshold)
 cv2.imshow("image", image)
 cv2.waitKey(0)
